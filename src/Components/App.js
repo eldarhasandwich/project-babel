c2be475020b7489b1d8176cc027fd964df1f0be3
eldarhasandwich <hasanovicel@gmail.com>
2018-03-02 08:04:36 +1100

    lets not use real peoples names

--- src/Components/App.js
+++ src/Components/App.js
@@ -15,74 +15,115 @@ class App extends Component {
             selectedClipIndex: 0,
             audioClipArray: [
                 {
-
+                    audioSrc: "https://freewavesamples.com/files/E-Mu-Proteus-FX-Wacky-Snare.wav",
+                    id: "00000001",
+                    name: "Mr Snare",
+                    textA: "Bachelor of Arts",
+                    textB: "N/A"
+                }, {
+                    audioSrc: "https://freewavesamples.com/files/Alesis-Fusion-Acoustic-Bass-C2.wav",
+                    id: "00000002",
+                    name: "Miss Acoustic",
+                    textA: "Bachelor of Engineering",
+                    textB: "N/A"
+                }, {
+                    audioSrc: "https://freewavesamples.com/files/Alesis-Fusion-Bass-C3.wav",
+                    id: "00000003",
+                    name: "Mrs Bass",
+                    textA: "Bachelor of Medicine",
+                    textB: "N/A"
                 }
             ]
         };
 
-        this.saveToLocalStorage = this.saveToLocalStorage.bind(this)
-        this.loadFromLocalStorage = this.loadFromLocalStorage.bind(this)
+        this.saveToLocalStorage = this
+            .saveToLocalStorage
+            .bind(this)
+        this.loadFromLocalStorage = this
+            .loadFromLocalStorage
+            .bind(this)
 
-        this.incrementIndex = this.incrementIndex.bind(this);
-        this.decrementIndex = this.decrementIndex.bind(this);
-        this.getSelectedClip = this.getSelectedClip.bind(this);
-        this.playSelectedAudio = this.playSelectedAudio.bind(this);
+        this.incrementIndex = this
+            .incrementIndex
+            .bind(this);
+        this.decrementIndex = this
+            .decrementIndex
+            .bind(this);
+        this.getSelectedClip = this
+            .getSelectedClip
+            .bind(this);
+        this.playSelectedAudio = this
+            .playSelectedAudio
+            .bind(this);
 
     }
 
-    loadFromLocalStorage () {
+    loadFromLocalStorage() {
         console.log("loadin'")
         var newState = Data.Read_From_JSON("Project-Babel-Store");
         this.setState(newState);
         alert("Data Loaded From File!")
     }
-    
-    saveToLocalStorage () {
+
+    saveToLocalStorage() {
         console.log("savin'")
         Data.Write_To_JSON(this.state, "Project-Babel-Store");
         alert("Data Saved To File!")
     }
 
-    addNewClip (object) {
+    addNewClip(object) {
         // TODO: clipObjectVerification
-        this.setState(
-            prevState => ({audioClipArray: [...prevState.audioClipArray, object]})
-        )
+        this.setState(prevState => ({
+            audioClipArray: [
+                ...prevState.audioClipArray,
+                object
+            ]
+        }))
     }
 
-    getSelectedClip () { 
-        return this.state.audioClipArray[ this.state.selectedClipIndex ] || null
+    getSelectedClip() {
+        return this.state.audioClipArray[this.state.selectedClipIndex] || null
     }
-    
-    boolCanDecrement () {
+
+    boolCanDecrement() {
         if (this.state.selectedClipIndex < 1) {
             return false;
-        } return true;
+        }
+        return true;
     }
-    
-    decrementIndex () {
-        if (!this.boolCanDecrement()) { return; }
-        this.setState(
-            prevState => ({selectedClipIndex: prevState.selectedClipIndex - 1})
-        );
+
+    decrementIndex() {
+        if (!this.boolCanDecrement()) {
+            return;
+        }
+        this.setState(prevState => ({
+            selectedClipIndex: prevState.selectedClipIndex - 1
+        }));
     }
-    
-    boolCanIncrement () {
+
+    boolCanIncrement() {
         if (this.state.selectedClipIndex > this.state.audioClipArray.length - 2) {
             return false;
-        } return true;
+        }
+        return true;
     }
 
-    incrementIndex () {
-        if (!this.boolCanIncrement()) { return; }
-        this.setState(
-            prevState => ({selectedClipIndex: prevState.selectedClipIndex + 1})
-        );
+    incrementIndex() {
+        if (!this.boolCanIncrement()) {
+            return;
+        }
+        this.setState(prevState => ({
+            selectedClipIndex: prevState.selectedClipIndex + 1
+        }));
     }
 
-    playSelectedAudio () {
-        var audioSrc = this.getSelectedClip().audioSrc;
-        if (audioSrc == null) { return; }
+    playSelectedAudio() {
+        var audioSrc = this
+            .getSelectedClip()
+            .audioSrc;
+        if (audioSrc == null) {
+            return;
+        }
         new Audio(audioSrc).play();
     }
 
@@ -92,48 +133,36 @@ class App extends Component {
 
                 <AppHeader/>
 
-                <button
-                    onClick={this.loadFromLocalStorage}
-                >
+                <button onClick={this.loadFromLocalStorage}>
                     Load From File
                 </button>
-                <button
-                    onClick={this.saveToLocalStorage}
-                >
+                <button onClick={this.saveToLocalStorage}>
                     Save To File
                 </button>
 
-                <ClipList 
+                <ClipList
                     cliparray={this.state.audioClipArray}
-                    selectedIndex={this.state.selectedClipIndex}
-                />
+                    selectedIndex={this.state.selectedClipIndex}/>
 
-                <SelectedClipInterface
-                    value={this.getSelectedClip()}
-                />
+                <SelectedClipInterface value={this.getSelectedClip()}/>
 
                 <div className="Interface-buttons">
 
-                    <button 
+                    <button
                         id="back-btn"
-                        onClick={this.decrementIndex} 
-                        disabled={!this.boolCanDecrement()}
-                    >
+                        onClick={this.decrementIndex}
+                        disabled={!this.boolCanDecrement()}>
                         Back
                     </button>
 
-                    <button
-                        id="play-btn"
-                        onClick={this.playSelectedAudio}
-                    >
+                    <button id="play-btn" onClick={this.playSelectedAudio}>
                         Play
                     </button>
 
                     <button
                         id="next-btn"
                         onClick={this.incrementIndex}
-                        disabled={!this.boolCanIncrement()}
-                    >
+                        disabled={!this.boolCanIncrement()}>
                         Next
                     </button>
 
