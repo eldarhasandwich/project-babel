c6ce1a15df191893adc77ff6adcdc89b7972ac64
eldarhasandwich <hasanovicel@gmail.com>
2018-03-21 10:57:15 +1100

    progress on recording modal

--- src/Components/AttendeeView/AudioRecordingModal.js
+++ src/Components/AttendeeView/AudioRecordingModal.js
@@ -1,30 +1,77 @@
 import React, {Component} from 'react';
 import Modal from 'react-modal'
 
+import './AudioRecordingModal.css'
+
 class AudioRecordingModal extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            audio: null,
+            recording: false
+        }
+    }
+
+    startRecording() {
+        this.setState({recording: true})
+    }
+
+    stopRecording() {
+        this.setState({recording: false})
+    }
+
+    submitRecording() {
+        console.log("recording submitted!")
+    }
 
     render() {
         return (
-          <div>
-            <Modal
-              isOpen={this.props.modalIsOpen}
-              onAfterOpen={this.afterOpenModal}
-              onRequestClose={this.props.closeModal}
-              contentLabel="Example Modal"
-            >
-     
-              <h2>Upload AudioClip</h2>
-              <button onClick={this.props.closeModal}>close</button>
-              <div>I am a modal</div>
-              <form>
-                <input />
-                <button>tab navigation</button>
-                <button>stays</button>
-                <button>inside</button>
-                <button>the modal</button>
-              </form>
-            </Modal>
-          </div>
+            <div>
+                <Modal
+                    isOpen={this.props.modalIsOpen}
+                    onAfterOpen={this.afterOpenModal}
+                    onRequestClose={this.props.closeModal}
+                    contentLabel="Example Modal">
+                    <div className="modal-div">
+                        <h2>Upload AudioClip</h2>
+                        <button onClick={this.props.closeModal}>close</button>
+
+                        <div className="oscilloscope">
+                            oscilloscope
+                        </div>
+
+                        <button
+                            disabled={this.state.audio === null} 
+                            id="modal-play-button">
+                            play btn
+                        </button>
+
+                        <div className="modal-btn-row">
+                            <button
+                                disabled={this.state.recording}
+                                onClick={this.startRecording.bind(this)} 
+                                id="modal-record-button">
+                                record btn
+                            </button>
+                            <button
+                                disabled={!this.state.recording}
+                                onClick={this.stopRecording.bind(this)}
+                                id="modal-stop-record-button">
+                                stop btn
+                            </button>
+                        </div>
+
+                        <button
+                            disabled={this.state.audio === null}
+                            id="modal-submit-clip-button">
+                            submit btn
+                        </button>
+                    </div>
+
+                </Modal>
+            </div>
         );
     }
 }
