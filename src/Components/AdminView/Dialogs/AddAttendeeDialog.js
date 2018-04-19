c0e98be077e6f7b216472d48cae8b1ee9075af78
eldarhasandwich <hasanovicel@gmail.com>
2018-04-20 09:46:13 +1000

    refactored add attendee dialog out of attendee table

---
+++ src/Components/AdminView/Dialogs/AddAttendeeDialog.js
@@ -0,0 +1,224 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux'
+import {RaisedButton, Toggle} from 'material-ui';
+
+import {FlatButton, Dialog, TextField} from 'material-ui';
+import {Tab, Tabs} from 'material-ui'
+import Dropzone from 'react-dropzone'
+
+import * as StateActions from '../../../Actions/state'
+import * as UserSessionActions from '../../../Actions/userSession'
+import CSVtoArray from '../../../Classes/CSVOperations'
+
+class AddAttendeeDialog extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            attendeeName: "",
+            attendeeEmail: "",
+            attendeeCSVImport: {
+                nameHeader: "",
+                emailHeader: "",
+                data: []
+            }
+        }
+    }
+
+    setNewAttendeeName = newName => {
+        this.setState({attendeeName: newName.target.value})
+    }
+
+    setNewAttendeeEmail = newEmail => {
+        this.setState({attendeeEmail: newEmail.target.value})
+    }
+
+    resetNewAttendee = () => {
+        this.setState({attendeeName: "", attendeeEmail: ""})
+    }
+
+    submitAttendeeInfo = () => {
+        if (this.state.attendeeName === "" || this.state.attendeeEmail === "") {
+            return;
+        }
+
+        this.closeCreateAttendeeDialog()
+        this
+            .props
+            .addNewAttendee(this.state.attendeeName, this.state.attendeeEmail)
+        this.resetNewAttendee()
+    }
+
+    submitAttendeeCSV = () => {
+        console.log("Import CSV button")
+
+        this.closeCreateAttendeeDialog()
+        // this.props.addNewAttendee(this.state.attendeeName, this.state.attendeeEmail)
+
+        this.state.attendeeCSVImport.data.forEach(
+            x => {
+                this.props.addNewAttendee(x[0], x[1])
+            }
+        )
+
+        this.resetAttendeeCSV()
+    }
+
+    onCSVDrop = (acceptedFiles, rejectedFiles) => {
+        // console.log(acceptedFiles) console.log("rejected file: " + rejectedFiles)
+        if (acceptedFiles === []) {
+            return;
+        }
+
+        console.log(acceptedFiles[acceptedFiles.length - 1])
+
+        const reader = new FileReader();
+        reader.onload = () => {
+            const fileAsBinaryString = reader.result;
+            let CSVdat = CSVtoArray(fileAsBinaryString)
+            console.log(CSVdat)
+
+            this.setState({
+                attendeeCSVImport: {
+                    nameHeader: CSVdat.nameHeader,
+                    emailHeader: CSVdat.emailHeader,
+                    data: CSVdat.data
+                }
+            })
+
+            console.log(this.state)
+
+        };
+        reader.onabort = () => console.log('file reading was aborted');
+        reader.onerror = () => console.log('file reading has failed');
+
+        reader.readAsBinaryString(acceptedFiles[acceptedFiles.length - 1]);
+    }
+
+    csvImportDescription = () => {
+        if (this.state.attendeeCSVImport.data.length === 0) {
+            return `Please select a CSV file`
+        }
+
+        return `The CSV file you are attempting to import uses "${this.state.attendeeCSVImport.nameHeader}" as the name field and "${this.state.attendeeCSVImport.emailHeader}" as the email field. This file has information for ${this.state.attendeeCSVImport.data.length} attendees.`
+    }
+
+
+    render() {
+
+        return (
+            <Dialog
+                title={"Add new Attendees to " + this.props.userSession.companyLists[this.props.userSession.selectedList].listName}
+                onRequestClose={this.props.onRequestClose} open={this.props.isOpen} >
+                <Tabs>
+                    <Tab
+                        label="Single Attendee"
+                        style={{
+                        backgroundColor: "white",
+                        color: "#222"
+                    }}>
+                        <div>
+                            <TextField
+                                floatingLabelText={"Attendee Name"}
+                                floatingLabelFixed={true}
+                                value={this.state.newAttendeeName}
+                                onChange={this.setNewAttendeeName}/>
+                        </div>
+                        <div>
+                            <TextField
+                                floatingLabelText={"Contact Email"}
+                                floatingLabelFixed={true}
+                                value={this.state.newAttendeeEmail}
+                                onChange={this.setNewAttendeeEmail}/>
+                        </div>
+                        <div
+                            style={{
+                            float: "right"
+                        }}>
+                            <FlatButton label={"Add Attendee"} primary onClick={this.submitAttendeeInfo}/>
+                        </div>
+                    </Tab>
+                    <Tab
+                        label="Import CSV"
+                        style={{
+                        backgroundColor: "white",
+                        color: "#222"
+                    }}>
+
+                        <div
+                            style={{
+                            width: "100%",
+                            overflow: "auto"
+                        }}>
+
+                            <div
+                                style={{
+                                marginTop: "20px",
+                                marginLeft: "30px",
+                                float: "left"
+                            }}>
+                                <Dropzone accept="text/csv, application/vnd.ms-excel" onDrop={this.onCSVDrop}>
+                                    <p
+                                        style={{
+                                        margin: "15px"
+                                    }}>
+                                        Drag a CSV file here or click to select one.
+                                    </p>
+                                </Dropzone>
+                            </div>
+
+                            <p
+                                style={{
+                                marginTop: "20px",
+                                marginLeft: "15px",
+                                float: "left",
+                                width: "90%"
+                            }}>
+                                {this.csvImportDescription()}
+                            </p>
+
+                        </div>
+
+                        <div
+                            style={{
+                            width: "100%",
+                            overflow: "auto"
+                        }}>
+                            <FlatButton
+                                style={{
+                                float: "right"
+                            }}
+                                label={"Import List"}
+                                primary
+                                onClick={this.submitAttendeeCSV}/>
+                        </div>
+
+                    </Tab>
+                    <Tab
+                        label="Import Excel"
+                        style={{
+                        backgroundColor: "white",
+                        color: "#222"
+                    }}>
+                        Theres nothing here! (yet)
+                    </Tab>
+                </Tabs>
+
+            </Dialog>
+        )
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {state: state.state, userSession: state.userSession}
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        addNewAttendee: (newAttendeeName, newAttendeeEmail) => dispatch(UserSessionActions.addNewAttendee(newAttendeeName, newAttendeeEmail))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddAttendeeDialog)
--- src/Components/AdminView/ListAttendeeTable.js
+++ src/Components/AdminView/ListAttendeeTable.js
@@ -10,11 +10,9 @@ import {
     TableRowColumn
 } from 'material-ui/Table';
 import {FlatButton, Dialog, TextField} from 'material-ui';
-import {Tab, Tabs} from 'material-ui'
-import Dropzone from 'react-dropzone'
 
 import * as UserSessionActions from '../../Actions/userSession'
-import CSVtoArray from '../../Classes/CSVOperations'
+import AddAttendeeDialog from './Dialogs/AddAttendeeDialog';
 
 class ListAttendeeTable extends Component {
 
@@ -22,14 +20,7 @@ class ListAttendeeTable extends Component {
         super(props)
 
         this.state = {
-            addAttendeeDialogOpen: false,
-            attendeeName: "",
-            attendeeEmail: "",
-            attendeeCSVImport: {
-                nameHeader: "",
-                emailHeader: "",
-                data: []
-            }
+            addAttendeeDialogOpen: false
         }
     }
 
@@ -39,18 +30,6 @@ class ListAttendeeTable extends Component {
 
     closeCreateAttendeeDialog = () => {
         this.setState({addAttendeeDialogOpen: false})
-    }
-
-    setNewAttendeeName = newName => {
-        this.setState({attendeeName: newName.target.value})
-    }
-
-    setNewAttendeeEmail = newEmail => {
-        this.setState({attendeeEmail: newEmail.target.value})
-    }
-
-    resetNewAttendee = () => {
-        this.setState({attendeeName: "", attendeeEmail: ""})
     }
 
     resetAttendeeCSV = () => {
@@ -106,33 +85,6 @@ class ListAttendeeTable extends Component {
         return attendeeKeys
     }
 
-    submitAttendeeInfo = () => {
-        if (this.state.attendeeName === "" || this.state.attendeeEmail === "") {
-            return;
-        }
-
-        this.closeCreateAttendeeDialog()
-        this
-            .props
-            .addNewAttendee(this.state.attendeeName, this.state.attendeeEmail)
-        this.resetNewAttendee()
-    }
-
-    submitAttendeeCSV = () => {
-        console.log("Import CSV button")
-
-        this.closeCreateAttendeeDialog()
-        // this.props.addNewAttendee(this.state.attendeeName, this.state.attendeeEmail)
-
-        this.state.attendeeCSVImport.data.forEach(
-            x => {
-                this.props.addNewAttendee(x[0], x[1])
-            }
-        )
-
-        this.resetAttendeeCSV()
-    }
-
     setSelectedAttendee = attendeeKey => {
         this
             .props
@@ -143,46 +95,7 @@ class ListAttendeeTable extends Component {
         let attendees = this.getSortedFilteredAttendees()
         this.setSelectedAttendee(attendees[index[0]])
     }
-
-    onCSVDrop = (acceptedFiles, rejectedFiles) => {
-        // console.log(acceptedFiles) console.log("rejected file: " + rejectedFiles)
-        if (acceptedFiles === []) {
-            return;
-        }
-
-        console.log(acceptedFiles[acceptedFiles.length - 1])
-
-        const reader = new FileReader();
-        reader.onload = () => {
-            const fileAsBinaryString = reader.result;
-            let CSVdat = CSVtoArray(fileAsBinaryString)
-            console.log(CSVdat)
-
-            this.setState({
-                attendeeCSVImport: {
-                    nameHeader: CSVdat.nameHeader,
-                    emailHeader: CSVdat.emailHeader,
-                    data: CSVdat.data
-                }
-            })
-
-            console.log(this.state)
-
-        };
-        reader.onabort = () => console.log('file reading was aborted');
-        reader.onerror = () => console.log('file reading has failed');
-
-        reader.readAsBinaryString(acceptedFiles[acceptedFiles.length - 1]);
-    }
-
-    csvImportDescription = () => {
-        if (this.state.attendeeCSVImport.data.length === 0) {
-            return `Please select a CSV file`
-        }
-
-        return `The CSV file you are attempting to import uses "${this.state.attendeeCSVImport.nameHeader}" as the name field and "${this.state.attendeeCSVImport.emailHeader}" as the email field. This file has information for ${this.state.attendeeCSVImport.data.length} attendees.`
-    }
-
+    
     render() {
         return (
             <div >
@@ -222,104 +135,10 @@ class ListAttendeeTable extends Component {
                     fullWidth
                     onClick={this.openCreateAttendeeDialog}/>
 
-                <Dialog
-                    title={"Add new Attendees to " + this.props.userSession.companyLists[this.props.userSession.selectedList].listName}
-                    onRequestClose={this.closeCreateAttendeeDialog}open={this.state.addAttendeeDialogOpen} >
-                    <Tabs>
-                        <Tab
-                            label="Single Attendee"
-                            style={{
-                            backgroundColor: "white",
-                            color: "#222"
-                        }}>
-                            <div>
-                                <TextField
-                                    floatingLabelText={"Attendee Name"}
-                                    floatingLabelFixed={true}
-                                    value={this.state.newAttendeeName}
-                                    onChange={this.setNewAttendeeName}/>
-                            </div>
-                            <div>
-                                <TextField
-                                    floatingLabelText={"Contact Email"}
-                                    floatingLabelFixed={true}
-                                    value={this.state.newAttendeeEmail}
-                                    onChange={this.setNewAttendeeEmail}/>
-                            </div>
-                            <div
-                                style={{
-                                float: "right"
-                            }}>
-                                <FlatButton label={"Add Attendee"} primary onClick={this.submitAttendeeInfo}/>
-                            </div>
-                        </Tab>
-                        <Tab
-                            label="Import CSV"
-                            style={{
-                            backgroundColor: "white",
-                            color: "#222"
-                        }}>
-
-                            <div
-                                style={{
-                                width: "100%",
-                                overflow: "auto"
-                            }}>
-
-                                <div
-                                    style={{
-                                    marginTop: "20px",
-                                    marginLeft: "30px",
-                                    float: "left"
-                                }}>
-                                    <Dropzone accept="text/csv, application/vnd.ms-excel" onDrop={this.onCSVDrop}>
-                                        <p
-                                            style={{
-                                            margin: "15px"
-                                        }}>
-                                            Drag a CSV file here or click to select one.
-                                        </p>
-                                    </Dropzone>
-                                </div>
-
-                                <p
-                                    style={{
-                                    marginTop: "20px",
-                                    marginLeft: "15px",
-                                    float: "left",
-                                    width: "90%"
-                                }}>
-                                    {this.csvImportDescription()}
-                                </p>
-
-                            </div>
-
-                            <div
-                                style={{
-                                width: "100%",
-                                overflow: "auto"
-                            }}>
-                                <FlatButton
-                                    style={{
-                                    float: "right"
-                                }}
-                                    label={"Import List"}
-                                    primary
-                                    onClick={this.submitAttendeeCSV}/>
-                            </div>
-
-                        </Tab>
-                        <Tab
-                            label="Import Excel"
-                            style={{
-                            backgroundColor: "white",
-                            color: "#222"
-                        }}>
-                            Theres nothing here! (yet)
-                        </Tab>
-                    </Tabs>
-
-                </Dialog>
+                <AddAttendeeDialog
+                    isOpen={this.state.addAttendeeDialogOpen}
+                    onRequestClose={this.closeCreateAttendeeDialog}
+                />
             </div>
         )
     }
