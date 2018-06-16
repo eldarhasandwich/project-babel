48d44d33343e1df3e9abb76f9ec1900b48e78d02
eldarhasandwich <hasanovicel@gmail.com>
2018-06-16 10:54:23 +1000

    gitignore

--- src/Classes/Fire.js
+++ src/Classes/Fire.js
@@ -2,14 +2,7 @@ import firebase from 'firebase'
 require("firebase/functions");
 
 //Initialize Firebase 
-var config = { 
-    apiKey: "AIzaSyCnSbKLiHhiH7_yLQwuaqUJQwDGItoILlk",
-    authDomain: "project-babel-datastore.firebaseapp.com", 
-    databaseURL: "https://project-babel-datastore.firebaseio.com", 
-    projectId: "project-babel-datastore", 
-    storageBucket: "project-babel-datastore.appspot.com", 
-    messagingSenderId: "970549376509"
-};
+var config = require("../config/firebase.json")
 
 var Fire = firebase.initializeApp(config);
 // let functions = firebase.functions()
